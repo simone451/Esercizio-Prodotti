e72754982627f94ba8bb4cf4045ddced05ac2d7e
simone <simone451>
2023-11-17 12:22:44 +0100

    inizializzazione hashmap

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -35,17 +35,35 @@ public class Main {
 } */
 
 package org.generation.italy;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String[] prodotti = {"pane", "pasta barilla","biscotti", "pasta divella", "mortadella"};
-		float[] prezzi = {4.0f, 1.60f, 3.75f, 12.50f, 11f};
-		String nomeProdotto;
-		int posizione, i;
-		float quantita, prezzo;
-		String risposta;
+        HashMap<Integer, Float> prezzi = new HashMap<>();
+        HashMap<Integer, String> prodotti = new HashMap<>();
+
+        // Inizializza la mappa dei prezzi direttamente
+        prezzi = new HashMap<Integer, Float>() { private static final long serialVersionUID = 1L;
+		{
+            put(1, 4.0f);
+            put(2, 1.60f);
+            put(3, 3.75f);
+            put(4, 12.50f);
+            put(5, 11.0f);
+        }};
+
+        // Inizializza la mappa dei prodotti in modo tradizionale
+        prodotti.put(1, "pane");
+        prodotti.put(2, "pasta barilla");
+        prodotti.put(3, "biscotti");
+        prodotti.put(4, "pasta divella");
+        prodotti.put(5, "mortadella");
+
+        int codiceProdotto;
+        float quantita, prezzo;
+        String risposta;
 
 		do {
 			System.out.println("inserisci nome prodotto");
